89ef5773c32fcde9699179c227e5c4d1152f763a
y-annik <y-annik@Yannik-PC>
2017-04-10 21:39:30 +0200

    speed optimization

--- UEB01 Sieb des Eratosthenes/src/Main.java
+++ UEB01 Sieb des Eratosthenes/src/Main.java
@@ -7,27 +7,6 @@ public class Main
 	{
 		SiebDesEratosthenes sieb = new SiebDesEratosthenes();
 		
-//		sieb.gebePrimzahlenAus(-5);
-//		sieb.gebePrimzahlenAus(0);
-//		sieb.gebePrimzahlenAus(1); // Ungültige Eingaben
-//		
-//		
-//		sieb.gebePrimzahlenAus(2);
-//		System.out.println();
-//		sieb.gebePrimzahlenAus(4);  // 3 nötig => n/2 + 1
-//		System.out.println();
-//		sieb.gebePrimzahlenAus(7); // 5 nötig => n/2 + 2
-//		System.out.println();
-//		sieb.gebePrimzahlenAus(10); // 6 nötig
-//		System.out.println();
-//		sieb.gebePrimzahlenAus(25); // 14 nötig
-//		
-//		System.out.println();
-//		sieb.gebePrimzahlenAus(100);
-//		System.out.println();
-//		sieb.gebePrimzahlenAus(1000);
-//		System.out.println();
-//		sieb.gebePrimzahlenAus(7500); // Maximalwert (nur innerhalb von Eclipse!)
 		
 //		Scanner sc = new Scanner(System.in);
 //		System.out.println("Geben Sie hier die gewünschte Obergrenze ein!");
@@ -36,12 +15,15 @@ public class Main
 //		System.out.println("Es werden nun alle Primzahlen von 1 bis " + eingabe + " angezeigt!");
 //		sieb.gebePrimzahlenAus(eingabe);
 		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Geben Sie hier die gewünschte Obergrenze ein!");
-		int eingabe = sc.nextInt();
-		sc.close();
-		System.out.println("Es wird nun die Anzahl aller Primzahlen von 1 bis " + eingabe + " angezeigt!");
-		System.out.println(sieb.berechneAnzPrimzahlen(eingabe));
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("Geben Sie hier die gewünschte Obergrenze ein!");
+//		int eingabe = sc.nextInt();
+//		sc.close();
+//		System.out.println("Es wird nun die Anzahl aller Primzahlen von 1 bis " + eingabe + " angezeigt!");
+//		System.out.println(sieb.berechneAnzPrimzahlen(eingabe));
+		
+		System.out.println(sieb.berechneAnzPrimzahlen(1000000000));
+//		sieb.gebePrimzahlenAus(1000000000);
 
 	}
 
--- UEB01 Sieb des Eratosthenes/src/SiebDesEratosthenes.java
+++ UEB01 Sieb des Eratosthenes/src/SiebDesEratosthenes.java
@@ -1,8 +1,9 @@
 public class SiebDesEratosthenes
 {
-	private int[][] sieb;
+	private boolean[] sieb;
 	private int n;
 
+	
 	public void gebePrimzahlenAus(int grenze)
 	{
 		if (grenze < 2)
@@ -14,11 +15,15 @@ public class SiebDesEratosthenes
 
 			for (int i = 0; i < sieb.length; i++)
 			{
-				if (sieb[i][1] != 2)
-					System.out.print(sieb[i][0] + " ");
+				if (!sieb[i])
+					if(i == 0)
+						System.out.print(2 + " ");
+					else
+						System.out.print(((2 * i) + 1) + " ");
 			}
 		}
 	}
+	
 	
 	public long berechneAnzPrimzahlen(int grenze)
 	{
@@ -33,49 +38,36 @@ public class SiebDesEratosthenes
 
 			for (int i = 0; i < sieb.length; i++)
 			{
-				if (sieb[i][1] != 2)
+				if (!sieb[i])
 					anz++;
 			}
 		}
 		return anz;
 	}
-
-	private int[][] findePrimzahlen()
-	{
+	
+	private void findePrimzahlen(){
+		
 		if(n % 2 == 0)
-			sieb = new int[n/2][2]; // Zahl, Status: 0 ist nichts, 1 ist gerahmt, 2 ist gestrichen
+			sieb = new boolean[n/2]; 
 		else
-			sieb = new int[n/2 + 1][2]; // bei ungeradem n wird eine Postion mehr benötigt
-
-		int pos = 0;
-		int zahl = 2;
-		for (zahl = 2; zahl <= n; zahl++)
-		{
-			if(zahl == 2 || zahl % 2 != 0){  // Nur ungerade Zahlen (Ausnahme 2)
-				sieb[pos][0] = zahl; // Zahlen eintragen ((pos+1) ist Zahl)
-				pos++;
-			}
-		}
-
-		pos = 1;
-		zahl = 3;
+			sieb = new boolean[n/2 + 1]; // bei ungeradem n wird eine Postion mehr benötigt (Beginn bei 2!!!)
+		
+		int pos = 1;
+		int zahl = 3;
 		int posVielfaches = pos + zahl;
 		do
 		{
-			if (sieb[pos][1] == 0) // Wenn Zahl nicht durchgestrichen oder gerahmt
+			if (!sieb[pos]) // Wenn Zahl nicht durchgestrichen
 			{
-				sieb[pos][1] = 1; // Zahl einrahmen
-				zahl = sieb[pos][0]; // Zahl speichern
-				
+				zahl = 2 * pos + 1;
 				for(posVielfaches = pos + zahl; posVielfaches < sieb.length; posVielfaches += zahl){
-					if(sieb[posVielfaches][1] == 0 ) // Wenn Zahl Vielfaches von eingerahmter Zahl nicht durchgestrichen oder gerahmt
-						sieb[posVielfaches][1] = 2; // Zahl durchstreichen
+					if(!sieb[posVielfaches] ) // Wenn Zahl Vielfaches und nicht durchgestrichen 
+						sieb[posVielfaches] = true; // Zahl durchstreichen
 				}
 			}
 			pos++;
 		}
 		while (zahl * zahl <= n);
-
-		return sieb;
 	}
+
 }
